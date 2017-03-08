8a95703ef52fbad21b599af452ff4b564eed85cd
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-03-08 12:51:25 -0800

    V1.0

--- registry.go
+++ registry.go
@@ -8,7 +8,7 @@ import (
 
 //go:generate protoc hreg.proto --go_out=plugins=grpc:.
 
-const registryAddress = "localhost:5410"
+const registryAddress = "hcr.steelcode.com:5410"
 
 func GetHCRClient() (HamiltonRegistryClient, error) {
 	conn, err := grpc.Dial(registryAddress, grpc.WithInsecure(), grpc.WithTimeout(5*time.Second), grpc.FailOnNonTempDialError(true), grpc.WithBlock())
--- registry_test.go
+++ registry_test.go
@@ -1,18 +1,259 @@
 package hcr
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"testing"
+
+	"github.com/pborman/uuid"
 )
+
+func TestGetMoteIDNoAuth(t *testing.T) {
+	cl, err := GetHCRClient()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	did := uuid.NewRandom().String()[:10]
+	rv, err := cl.GetMoteID(context.Background(), &GetMoteIDParams{
+		DeviceId: did,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if rv.Status.Okay {
+		t.Fatalf("expected bad status, got %v %v", rv, err)
+	}
+}
 
 func TestGetMoteID(t *testing.T) {
 	cl, err := GetHCRClient()
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+	did := uuid.NewRandom().String()[:10]
 	rv, err := cl.GetMoteID(context.Background(), &GetMoteIDParams{
-		DeviceId: "foobar",
+		DeviceId: did,
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
 	})
-	fmt.Printf("here %v %v\n", rv, err)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv, err)
+	}
+
+	rv2, err := cl.GetMoteID(context.Background(), &GetMoteIDParams{
+		DeviceId: did,
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv2.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv2, err)
+	}
+	if rv2.Mac != rv.Mac || rv2.Id != rv.Id {
+		t.Fatalf("expected same ID")
+	}
+}
+
+func TestCreateDeployment(t *testing.T) {
+	cl, err := GetHCRClient()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	name := uuid.NewRandom().String()[:10]
+	rv, err := cl.CreateDeployment(context.Background(), &CreateDeploymentParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Name: name,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv, err)
+	}
+}
+func TestCreateDuplicateDeployment(t *testing.T) {
+	cl, err := GetHCRClient()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	name := uuid.NewRandom().String()[:10]
+	rv, err := cl.CreateDeployment(context.Background(), &CreateDeploymentParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Name: name,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv, err)
+	}
+	rv, err = cl.CreateDeployment(context.Background(), &CreateDeploymentParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Name: name,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if rv.Status.Okay {
+		t.Fatalf("expected error status, got %v,%v", rv, err)
+	}
+}
+func TestBindBadId(t *testing.T) {
+	cl, err := GetHCRClient()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	name := uuid.NewRandom().String()[:10]
+	rv, err := cl.CreateDeployment(context.Background(), &CreateDeploymentParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Name: name,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv, err)
+	}
+	rv2, err := cl.BindMote(context.Background(), &BindMoteParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Deployment: name,
+		Moteid:     0x1234,
+	})
+	fmt.Printf("got %v %v\n", rv2, err)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if rv2.Status.Okay {
+		t.Fatalf("expected error status, got %v,%v", rv2, err)
+	}
+}
+
+func TestBindBadDid(t *testing.T) {
+	cl, err := GetHCRClient()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	name := uuid.NewRandom().String()[:10]
+	rv, err := cl.CreateDeployment(context.Background(), &CreateDeploymentParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Name: name,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv, err)
+	}
+	rv2, err := cl.BindMote(context.Background(), &BindMoteParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Deployment: "not a real deployment",
+		Moteid:     0x100,
+	})
+	fmt.Printf("got %v %v\n", rv2, err)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if rv2.Status.Okay {
+		t.Fatalf("expected error status, got %v,%v", rv2, err)
+	}
+}
+
+func TestMoteInfo(t *testing.T) {
+	cl, err := GetHCRClient()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	name := uuid.NewRandom().String()[:10]
+	rv, err := cl.CreateDeployment(context.Background(), &CreateDeploymentParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Name: name,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rv.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rv, err)
+	}
+	did := uuid.NewRandom().String()[:10]
+	rvm, err := cl.GetMoteID(context.Background(), &GetMoteIDParams{
+		DeviceId: did,
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rvm.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rvm, err)
+	}
+	rvb, err := cl.BindMote(context.Background(), &BindMoteParams{
+		Auth: &Auth{
+			UserSecret: "764efa883dda1e11db47671c4a3bbd9e",
+		},
+		Deployment: name,
+		Moteid:     rvm.Id,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rvb.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rvb, err)
+	}
+
+	cir, err := cl.CreateInstance(context.Background(), &CreateInstanceParams{
+		Auth: &Auth{
+			DeploymentSecret: rv.WriteKey,
+		},
+		DeviceId:   did,
+		Repository: "foo",
+		Commit:     "bar",
+		Motetype:   0x3c,
+		Extradata:  "foo",
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !cir.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", cir, err)
+	}
+
+	rvi, err := cl.MoteInfo(context.Background(), &MoteInfoParams{
+		Auth: &Auth{
+			DeploymentSecret: rv.ReadKey,
+		},
+		Moteid: rvm.Id})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !rvi.Status.Okay {
+		t.Fatalf("expected okay status, got %v,%v", rvi, err)
+	}
+	if !bytes.Equal(rvi.AESK, cir.AESK) {
+		t.Fatalf("AES key mismatch got\n%064x expected\n%064x", rvi.AESK, cir.AESK)
+	}
 }
